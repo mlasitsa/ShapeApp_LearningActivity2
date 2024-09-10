660004fa0713706a31a609c6a1179f1bde7f824a
mlasitsa <mlasitsa@LIBCKOUT08-ETYC>
2024-09-10 10:18:33 -0500

    Add project files.

---
+++ ShapeApp_LearningActivity2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace Shape_LearningActivity2
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.setWidth(3);
+            rectangle.setHeight(4);
+            rectangle.calculateArea();
+
+            Triangle triangle = new Triangle();
+            triangle.setWidth(3);
+            triangle.setHeight(4);
+            triangle.calculateArea();
+
+            if (rectangle.CompareTo(triangle) < 0)
+            {
+                Console.WriteLine("The rectangle is less than the triangle.");
+            }
+            else if (rectangle.CompareTo(triangle) == 0)
+            {
+                Console.WriteLine("The rectangle is equal to the triangle.");
+            }
+            else
+            {
+                Console.WriteLine("The rectangle is greater than the triangle.");
+            }
+        }
+    }
+}
